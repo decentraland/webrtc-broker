5df08906d7547b5edbae3add274286a17f663f37
Hugo Arregui <hugo@decentraland.org>
2019-08-27 16:22:17 -0300

    new pion logging

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ require (
 	github.com/gorilla/websocket v1.4.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pion/datachannel v1.4.5
-	github.com/pion/logging v0.2.2
+	github.com/pion/logging v0.2.3-0.20190827191644-fa022e4cb84c
+	github.com/pion/logging/thirdparty/zerolog v0.0.0-20190827191644-fa022e4cb84c
 	github.com/pion/webrtc/v2 v2.1.3-0.20190823181802-2b314c021f84
-	github.com/rs/zerolog v1.14.3
+	github.com/rs/zerolog v1.15.0
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -5,6 +5,7 @@ import (
 	"runtime/debug"
 
 	pionlogging "github.com/pion/logging"
+	pionzerolog "github.com/pion/logging/thirdparty/zerolog"
 	"github.com/rs/zerolog"
 )
 
@@ -27,32 +28,6 @@ func LogPanic(log Logger) {
 	}
 }
 
-type levelLogger struct {
-	log Logger
-}
-
-func (ll *levelLogger) Trace(msg string) { ll.log.Debug().Msg(msg) }
-func (ll *levelLogger) Error(msg string) { ll.log.Error().Msg(msg) }
-func (ll *levelLogger) Debug(msg string) { ll.log.Debug().Msg(msg) }
-func (ll *levelLogger) Info(msg string)  { ll.log.Info().Msg(msg) }
-func (ll *levelLogger) Warn(msg string)  { ll.log.Warn().Msg(msg) }
-
-func (ll *levelLogger) Tracef(format string, args ...interface{}) {
-	ll.log.Debug().Msgf(format, args...)
-}
-func (ll *levelLogger) Debugf(format string, args ...interface{}) {
-	ll.log.Debug().Msgf(format, args...)
-}
-func (ll *levelLogger) Infof(format string, args ...interface{}) {
-	ll.log.Info().Msgf(format, args...)
-}
-func (ll *levelLogger) Warnf(format string, args ...interface{}) {
-	ll.log.Warn().Msgf(format, args...)
-}
-func (ll *levelLogger) Errorf(format string, args ...interface{}) {
-	ll.log.Error().Msgf(format, args...)
-}
-
 // PionLoggingFactory is the log factory expected by pion
 type PionLoggingFactory struct {
 	PeerAlias uint64
@@ -60,6 +35,11 @@ type PionLoggingFactory struct {
 
 // NewLogger creates a new logger for the given scope
 func (f *PionLoggingFactory) NewLogger(scope string) pionlogging.LeveledLogger {
-	log := New().Level(zerolog.ErrorLevel).With().Uint64("peer", f.PeerAlias).Logger()
-	return &levelLogger{log: log}
+	log := New().Level(zerolog.ErrorLevel).With().Uint64("peer", f.PeerAlias).Logger().Level(zerolog.DebugLevel)
+	return &pionlogging.Logger{
+		FormatterFactory: func(lvl pionlogging.LogLevel) pionlogging.Formatter {
+			return pionzerolog.NewZerologFormatter(log, lvl)
+		},
+		Lvl: pionlogging.LogLevelTrace,
+	}
 }
